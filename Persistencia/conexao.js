2d8ede1304adbc3bf97717b367b55512553ea960
jefoliveira <104362563+jefoliveira22@users.noreply.github.com>
2023-12-01 10:04:20 -0300

    Update conexao.js

--- Persistencia/conexao.js
+++ Persistencia/conexao.js
@@ -9,7 +9,7 @@ export default async function conectar() {
     const conexao = await mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "F4gnu6j2i",
+        password: "",
         database: "hotellegal"
     });
 
